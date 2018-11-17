5e816dd862127795788f1ab75b1cfce291c42556
Hamood3Amine <44638712+Hamood3Amine@users.noreply.github.com>
2018-11-17 20:11:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const user = new Discord.Client();
+const mem = new Discord.Client();
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -12,6 +14,15 @@ user.on('ready', () => {
 
 
 client.on("guildMemberAdd", member => {
+let welcomer = member.guild.channels.find("name","dalida");
+      if(!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var h = member.user;
+     welcomer.send(`**Welcome To __Dalida__ Server ..**`);          
+      }
+});
+mem.on("guildMemberAdd", member => {
 let welcomer = member.guild.channels.find("name","dalida");
       if(!welcomer) return;
       if(welcomer) {
@@ -33,3 +44,4 @@ let welcomer = member.guild.channels.find("name","dalida");
 
 client.login(process.env.client);
 user.login(process.env.user);
+mem.login(process.env.mem);
