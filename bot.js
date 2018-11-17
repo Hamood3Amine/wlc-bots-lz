c72e6948652d574a6f84d0a86576dd6b4b81c43a
Hamood3Amine <44638712+Hamood3Amine@users.noreply.github.com>
2018-11-17 20:09:26 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const user = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+user.on('ready', () => {
+  console.log(`Logged in as ${user.user.tag}!`);
+});
+
+
+
+client.on("guildMemberAdd", member => {
+let welcomer = member.guild.channels.find("name","dalida");
+      if(!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var h = member.user;
+     welcomer.send(`**Welcome To __Dalida__ Server ..**`);          
+      }
+});
+ user.on("guildMemberAdd", member => {
+let welcomer = member.guild.channels.find("name","dalida");
+      if(!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var h = member.user;
+     welcomer.send(`**Welcome To __Dalida__ Server ..**`);          
+      }
+});
+
+
+client.login(process.env.client);
+user.login(process.env.user);
